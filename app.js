a2b6bc6e4e9404ad6cc6de9804dce21094e594bd
Tara Chand Kumawat <tarachand122333@gmail.com>
2025-08-27 20:05:24 +0530

    initialize project with Express server and basic routes

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/repos/:username", async (req, res) => {
+  const username = req.params.username;
+  if (!username) {
+    return res.status(400).json({ error: "Username is required" });
+  }
+
+  console.log(`Fetching repos for user: ${username}`);
+
+  const response = await fetch("https://api.github.com/users/" + username);
+  const data = await response.json();
+
+  res.status(200).json(data);
+});
+
+async function start() {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+start().catch((err) => {
+  console.error("Error starting the server:", err);
+});
